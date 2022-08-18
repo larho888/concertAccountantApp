12ddf1aa898cf5c2f3fbbbd868d1f50a00a56d00
Kwabena <kwabenao2k@gmail.com>
2022-08-18 15:31:37 -0400

    added api key

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+// api key 0TsZKUciU5HKm4ylnIBkwVoD8U4aPAgY
 
 function App() {
   return (
