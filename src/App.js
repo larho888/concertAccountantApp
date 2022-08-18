72fc371747b909c2cb111d1d0304f4af6f354869
Kwabena <kwabenao2k@gmail.com>
2022-08-18 16:52:08 -0400

    Finished pseudocode and changed favicon file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from "./logo.svg";
+import axios from "axios";
 import "./App.css";
 
-// api key 0TsZKUciU5HKm4ylnIBkwVoD8U4aPAgY
+const key = `0TsZKUciU5HKm4ylnIBkwVoD8U4aPAgY`;
 
 function App() {
   return (
@@ -25,3 +26,24 @@ function App() {
 }
 
 export default App;
+
+// PSUEDOCODE
+
+//initialize firebase
+// Create state items to hold data from the API and user input
+// Create a component to display the information
+// Create a component to hold userinput using "getDatabase" method
+// Create a function to make the API call with the correct search parameters
+// Userinput is linked to API call where user selects which items get added to the list
+// This data is then sent to firebase
+// Display the list the user makes as a public list
+// Complete published lists page broken down by price
+// Create a function to handle the remove, getDatabase and reference. getDatabase and reference would allow for the user to delete items from the list
+// Error handling where if the user enters an invalid concert/show an alert pops up and if the user enters a negative budget
+
+// STRETCH GOALS:
+// Profanity API to allow appropiate names for the list
+// Sorting and filtering search results
+// Add a chart to show cost trends across multiple lists
+// Pagination for search results
+// Allow for the private list to be authenticated through google
