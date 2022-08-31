141382e88b4b81827fd45dade078d3668db0576e
ElijahW <elijahwalk.r@gmail.com>
2022-08-31 17:12:34 -0400

    imported componenet in app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.scss";
 import Header from "./components/Header";
 import SearchResults from "./components/SearchResults";
 import GetList from "./components/GetList";
+import GetPrivateList from "./components/GetPrivateList";
 import Login from "./components/Login";
 import { Routes, Route } from "react-router-dom"
 
